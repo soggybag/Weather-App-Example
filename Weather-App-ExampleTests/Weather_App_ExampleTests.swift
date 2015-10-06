5a81782b92b2f4e6262ca41405ae4a0ac6c8a888
Mitchell Hudson <soggybag@gmail.com>
2015-10-06 09:00:02 -0700

    Initial Commit

---
+++ Weather-App-ExampleTests/Weather_App_ExampleTests.swift
@@ -0,0 +1,36 @@
+//
+//  Weather_App_ExampleTests.swift
+//  Weather-App-ExampleTests
+//
+//  Created by mitchell hudson on 10/6/15.
+//  Copyright © 2015 mitchell hudson. All rights reserved.
+//
+
+import XCTest
+@testable import Weather_App_Example
+
+class Weather_App_ExampleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
